49267814ddbcd73f78eb72c8195f41da33ae5ba7
@hexlet/cli <support@hexlet.io>
2022-11-13 00:12:25 +0300

    submit java-web-ru/orm

---
+++ java-web-ru/orm/src/main/java/exercise/domain/Article.java
@@ -0,0 +1,50 @@
+package exercise.domain;
+
+import io.ebean.Model;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import io.ebean.annotation.NotNull;
+
+@Entity
+public class Article extends Model {
+
+    @Id
+    private long id;
+
+    private String title;
+
+    @Lob
+    private String body;
+
+    @ManyToOne
+    @NotNull
+    private Category category;
+
+    // BEGIN
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public Article(String title, String body, Category category) {
+        this.title = title;
+        this.body = body;
+        this.category = category;
+    }
+
+    // END
+}
