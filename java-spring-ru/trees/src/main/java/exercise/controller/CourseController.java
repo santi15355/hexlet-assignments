05e7d0f2bcfeca074805612bb59f1977a2654adf
@hexlet/cli <support@hexlet.io>
2022-12-12 12:24:36 +0300

    submit java-spring-ru/trees

---
+++ java-spring-ru/trees/src/main/java/exercise/controller/CourseController.java
@@ -0,0 +1,46 @@
+package exercise.controller;
+
+import exercise.model.Course;
+import exercise.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping("/courses")
+public class CourseController {
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @GetMapping(path = "")
+    public Iterable<Course> getCourses() {
+        return courseRepository.findAll();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Course getCourse(@PathVariable long id) {
+        return courseRepository.findById(id);
+    }
+
+    // BEGIN
+    @GetMapping(path = "/{id}/previous")
+    public Iterable<Course> getPreviousCourses(@PathVariable long id) {
+        Course course = courseRepository.findById(id);
+        if (course.getPath() == null) {
+            return new ArrayList<>();
+        }
+        List<Long> result = Arrays.stream(course.getPath().split("\\."))
+                .mapToLong(Long::parseLong)
+                .boxed().toList();
+        return courseRepository.findAllById(result);
+    }
+    // END
+
+}
