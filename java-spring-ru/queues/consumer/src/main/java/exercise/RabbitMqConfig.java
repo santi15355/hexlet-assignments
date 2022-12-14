686e85f3badc15aee08f04097f456e30f68c1d45
@hexlet/cli <support@hexlet.io>
2022-12-14 18:04:58 +0300

    submit java-spring-ru/queues

---
+++ java-spring-ru/queues/consumer/src/main/java/exercise/RabbitMqConfig.java
@@ -0,0 +1,19 @@
+package exercise;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    // BEGIN
+    @Bean
+    Queue queue() {
+        return new Queue("queue", false);
+    }
+    // END
+}
---
+++ java-spring-ru/queues/producer/src/main/java/exercise/RabbitMqConfig.java
@@ -0,0 +1,29 @@
+package exercise;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMqConfig {
+
+    // BEGIN
+    @Bean
+    Queue queue() {
+        return new Queue("queue", false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange("exchange");
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("key");
+    }
+    // END
+}
